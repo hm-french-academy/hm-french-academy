a0af40866181b1338bf8a1036b684497dc3c1e4c
hm-french-academy <hf.toma@gmail.com>
2026-08-08 22:24:39 +0300

    Add controller service registry foundation

---
+++ backend/src/controllers/serviceRegistry.js
@@ -0,0 +1,13 @@
+const userService = require('../services/userService');
+const courseService = require('../services/courseService');
+const lessonService = require('../services/lessonService');
+const assessmentService = require('../services/assessmentService');
+const progressService = require('../services/progressService');
+
+module.exports = {
+  userService,
+  courseService,
+  lessonService,
+  assessmentService,
+  progressService
+};
