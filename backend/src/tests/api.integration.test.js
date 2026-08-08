c71925a5e649983e25cfbd453747ad4cd467acb1
hm-french-academy <hf.toma@gmail.com>
2026-08-08 22:40:54 +0300

    Add backend API integration test foundation

---
+++ backend/src/tests/api.integration.test.js
@@ -0,0 +1,11 @@
+// HM Academy API integration test foundation
+
+const checks = [
+  'user flow',
+  'course flow',
+  'lesson flow',
+  'assessment flow',
+  'progress flow'
+];
+
+module.exports = checks;
