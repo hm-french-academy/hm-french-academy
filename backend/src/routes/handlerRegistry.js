042667d0c492f66993714c03d1ef5361ce0d1660
hm-french-academy <hf.toma@gmail.com>
2026-08-08 22:34:19 +0300

    Add API handler registry foundation

---
+++ backend/src/routes/handlerRegistry.js
@@ -0,0 +1,13 @@
+const userHandler = require('../api/handlers/userHandler');
+const courseHandler = require('../api/handlers/courseHandler');
+const lessonHandler = require('../api/handlers/lessonHandler');
+const assessmentHandler = require('../api/handlers/assessmentHandler');
+const progressHandler = require('../api/handlers/progressHandler');
+
+module.exports = {
+  userHandler,
+  courseHandler,
+  lessonHandler,
+  assessmentHandler,
+  progressHandler
+};
