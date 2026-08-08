f21d1d19d7eaa700328d80076e5c483f91aebc05
hm-french-academy <hf.toma@gmail.com>
2026-08-08 21:54:46 +0300

    Add user model structure

---
+++ backend/src/models/User.js
@@ -0,0 +1,11 @@
+class User {
+  constructor({ id, name, email, passwordHash, role = 'student' }) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.passwordHash = passwordHash;
+    this.role = role;
+  }
+}
+
+module.exports = User;
