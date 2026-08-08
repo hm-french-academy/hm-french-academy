ab2f4cce51851d2c64afd50e7c10bb249c511c78
hm-french-academy <hf.toma@gmail.com>
2026-08-08 07:06:46 +0300

    Add client learning progress controller

---
+++ scripts/learning-progress.js
@@ -0,0 +1,36 @@
+// HM Academy learning progress controller
+const HMProgress = {
+  storageKey: 'hm_academy_progress',
+
+  get(){
+    const saved = localStorage.getItem(this.storageKey);
+    return saved ? JSON.parse(saved) : {
+      xp: 0,
+      completedLessons: [],
+      achievements: []
+    };
+  },
+
+  save(data){
+    localStorage.setItem(this.storageKey, JSON.stringify(data));
+  },
+
+  completeLesson(id, xp = 50){
+    const data = this.get();
+    if(!data.completedLessons.includes(id)){
+      data.completedLessons.push(id);
+      data.xp += xp;
+    }
+    this.save(data);
+    return data;
+  },
+
+  addAchievement(id){
+    const data = this.get();
+    if(!data.achievements.includes(id)){
+      data.achievements.push(id);
+    }
+    this.save(data);
+    return data;
+  }
+};
