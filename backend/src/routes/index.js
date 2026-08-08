107e73413c3197851db681ef39c8bb7aa860bda5
hm-french-academy <hf.toma@gmail.com>
2026-08-08 21:56:03 +0300

    Add API routes foundation

---
+++ backend/src/routes/index.js
@@ -0,0 +1,10 @@
+const routes = {
+  auth: '/auth',
+  users: '/users',
+  courses: '/courses',
+  lessons: '/lessons',
+  assessments: '/assessments',
+  progress: '/progress'
+};
+
+module.exports = routes;
