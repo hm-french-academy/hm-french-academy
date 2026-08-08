bb696a313a6878fdb52cce64234acd7a072973d2
hm-french-academy <hf.toma@gmail.com>
2026-08-08 21:54:58 +0300

    Add lesson model structure

---
+++ backend/src/models/Lesson.js
@@ -0,0 +1,10 @@
+class Lesson {
+  constructor({ id, courseId, title, content }) {
+    this.id = id;
+    this.courseId = courseId;
+    this.title = title;
+    this.content = content;
+  }
+}
+
+module.exports = Lesson;
