2b8b3523b34f5295939dc29a84e1c73cdddca9a3
hm-french-academy <hf.toma@gmail.com>
2026-08-08 22:19:45 +0300

    Connect backend routes to API server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    name: 'HM Academy API',
-    status: 'running',
-    version: '1.0.0'
-  }));
+  res.setHeader('Content-Type', 'application/json');
+
+  if (req.url === '/api') {
+    res.writeHead(200);
+    return res.end(JSON.stringify({
+      name: 'HM Academy API',
+      status: 'running',
+      version: '1.0.0',
+      routes
+    }));
+  }
+
+  res.writeHead(404);
+  res.end(JSON.stringify({ message: 'Endpoint not found' }));
 });
 
 server.listen(PORT, () => {
