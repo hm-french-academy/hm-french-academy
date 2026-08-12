1ac5a1b596d56e60943f58fb7740b914ea230648
hm-french-academy <hf.toma@gmail.com>
2026-08-12 23:30:55 +0300

    Update production-config.js

--- data/production-config.js
+++ data/production-config.js
@@ -1,8 +1,8 @@
 // HM Academy production configuration.
 // This file contains PUBLIC Supabase client values only. Never place a service-role key here.
 window.HMProductionConfig = window.HMProductionConfig || {
-  enabled: false,
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  enabled: true,
+supabaseUrl: 'https://cxvvxgxwxxmyqukxhysx.supabase.co',
+supabaseAnonKey: 'sb_publishable_tySWqDwz3CgNnR1wQzHpTg_dZuudly9',
   analyticsFunctionUrl: ''
 };
