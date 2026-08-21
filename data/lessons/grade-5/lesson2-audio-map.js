6a2fa92660443ca08fff3fe977ecc5de6e85f470
hm-french-academy <hf.toma@gmail.com>
2026-08-21 22:33:14 +0300

    feat: define lesson 2 vocabulary and example audio map

---
+++ data/lessons/grade-5/lesson2-audio-map.js
@@ -0,0 +1,4 @@
+window.HMGrade5L02AudioMap={
+ 'un-stylo':'un stylo','une-gomme':'une gomme','des-stylos':'des stylos','le-stylo':'le stylo','la-gomme':'la gomme','les-stylos':'les stylos','l-animal':'l’animal',
+ examples:{'un-stylo':'J’ai un stylo.','une-gomme':'J’ai une gomme.','des-stylos':'J’ai des stylos.','le-stylo':'Le stylo est bleu.','la-gomme':'La gomme est rose.','les-stylos':'Les stylos sont sur la table.','l-animal':'L’animal est petit.'}
+};
